78ca690695a342c9cf47c1ca8a4cabed37333e84
AydenLarsen <124610038+AydenLarsen@users.noreply.github.com>
2023-04-14 11:10:14 -0600

    Update main.js

--- main.js
+++ main.js
@@ -2,11 +2,14 @@ let numCookie = 1000;
 let cookieAmount = document.getElementById("cookie-amount");
 let cookieClick = 1;
 let cps = 0;
+let buy1Cost = 10;
+let buy1CPS = 0.1;
+let buy1CostAmount = document.getElementById("buy1-cost");
 let upg1Cost = 100;
-let upg1CPS = 0.1;
 let upg1CostAmount = document.getElementById("upg1-cost");
 
 document.getElementById("btn").addEventListener("click", btnClicked);
+document.getElementById("buy1").addEventListener("click", buy1Clicked);
 document.getElementById("upg1").addEventListener("click", upg1Clicked);
 
 function update() {
@@ -28,13 +31,24 @@ function btnClicked() {
   update();
 }
 
-function upg1Clicked() {
-  if (numCookie >= upg1Cost) {
-    numCookie -= upg1Cost;
-    upg1Cost *= 1.3;
-    upg1Cost = Math.round(upg1Cost);
-    upg1CostAmount.innerHTML = upg1Cost;
-    cps += upg1CPS;
+function buy1Clicked() {
+  if (numCookie >= buy1Cost) {
+    numCookie -= buy1Cost;
+    buy1Cost *= 1.3;
+    buy1Cost = Math.round(buy1Cost);
+    buy1CostAmount.innerHTML = buy1Cost;
+    cps += buy1CPS;
     update();
   }
+}
+
+function upg1Clicked () {
+ if (numCookie >= upg1Cost) {
+numCookie -= upg1Cost;
+upg1Cost *= 5;
+upg1Cost = Math.round(upg1Cost);
+upg1CostAmount.innerHTML = upg1Cost;
+buy1CPS *= 2;
+update()
+}
 }
